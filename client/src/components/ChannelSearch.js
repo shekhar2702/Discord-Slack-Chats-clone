168385f7f67dc84a2ed5aaf6853315e6573ff516
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-27 15:37:20 +0530

    EditChannel component and related functionalities added.

--- client/src/components/ChannelSearch.js
+++ client/src/components/ChannelSearch.js
@@ -1,29 +1,64 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
 
+import { ResultsDropdown } from "./";
 import { SearchIcon } from "../assets";
 
-const ChannelSearch = () => {
+const ChannelSearch = ({ setToggleContainer }) => {
+  const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
+
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
+    }
+  }, [query]);
 
   const getChannels = async (text) => {
     try {
-      //TODO->Fetch channels
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: { $autocomplete: text },
+        members: { $in: [client.userID] },
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text },
+      });
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
     } catch (error) {
       setQuery("");
     }
   };
+
   const onSearch = (event) => {
     event.preventDefault();
-    setQuery(event.target.value);
+
     setLoading(true);
+    setQuery(event.target.value);
     getChannels(event.target.value);
   };
+
+  const setChannel = (channel) => {
+    setQuery("");
+    setActiveChannel(channel);
+  };
+
   return (
     <div className="channel-search__container">
       <div className="channel-search__input__wrapper">
-        <div className="channel-search__input__icon">
+        <div className="channel-serach__input__icon">
           <SearchIcon />
         </div>
         <input
@@ -34,7 +69,18 @@ const ChannelSearch = () => {
           onChange={onSearch}
         />
       </div>
+      {query && (
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   );
 };
+
 export default ChannelSearch;
