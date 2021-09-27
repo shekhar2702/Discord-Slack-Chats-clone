a21e89b7ff35e57cf114a33ac6b9722abd357159
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-27 14:55:53 +0530

    updated userlist,channelList,createChannel components

--- client/src/components/CreateChannel.js
+++ client/src/components/CreateChannel.js
@@ -22,14 +22,37 @@ const ChannelNameInput = ({ channelName = "", setChannelName }) => {
 };
 
 const CreateChannel = ({ createType, setIsCreating }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.userID || ""]);
   const [channelName, setChannelName] = useState("");
+
+  const createChannel = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newChannel = await client.channel(createType, channelName, {
+        name: channelName,
+        members: selectedUsers,
+      });
+
+      await newChannel.watch();
+
+      setChannelName("");
+      setIsCreating(false);
+      setSelectedUsers([client.userID]);
+      setActiveChannel(newChannel);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="create-channel__container">
       <div className="create-channel__header">
         <p>
           {createType === "team"
-            ? "Create a new channel"
-            : "Send a direct message"}
+            ? "Create a New Channel"
+            : "Send a Direct Message"}
         </p>
         <CloseCreateChannel setIsCreating={setIsCreating} />
       </div>
@@ -39,7 +62,12 @@ const CreateChannel = ({ createType, setIsCreating }) => {
           setChannelName={setChannelName}
         />
       )}
-      <UserList />
+      <UserList setSelectedUsers={setSelectedUsers} />
+      <div className="create-channel__button-wrapper" onClick={createChannel}>
+        <p>
+          {createType === "team" ? "Create Channel" : "Create Message Group"}
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/components/TeamChannelPreview.js
+++ client/src/components/TeamChannelPreview.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
-const TeamChannelPreview = () => {
+
+const TeamChannelPreview = ({
+  setActiveChannel,
+  setIsCreating,
+  setIsEditing,
+  setToggleContainer,
+  channel,
+  type,
+}) => {
   const { channel: activeChannel, client } = useChatContext();
+
   const ChannelPreview = () => (
     <p className="channel-preview__item">
       # {channel?.data?.name || channel?.data?.id}
     </p>
   );
+
   const DirectPreview = () => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id !== client.userID
     );
+
+    console.log(members[0]);
+
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={member[0]?.user?.fullName}
+          name={members[0]?.user?.fullName || members[0]?.user?.id}
           size={24}
         />
-        <p>{members[0]?.user?.fullName}</p>
+        <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
       </div>
     );
   };
+
   return (
     <div
       className={
@@ -30,7 +44,12 @@ const TeamChannelPreview = () => {
           : "channel-preview__wrapper"
       }
       onClick={() => {
-        console.log(channel);
+        setIsCreating(false);
+        setIsEditing(false);
+        setActiveChannel(channel);
+        if (setToggleContainer) {
+          setToggleContainer((prevState) => !prevState);
+        }
       }}
     >
       {type === "team" ? <ChannelPreview /> : <DirectPreview />}
